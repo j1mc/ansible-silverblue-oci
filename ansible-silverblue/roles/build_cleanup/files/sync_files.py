b0109b58ecdec7c1672ae26b583131964855681b
Jim Campbell <jim@ansiblefacts.com>
2023-01-29 13:43:03 -0600

    Fix handling of onepassword

--- ansible-silverblue/roles/build_cleanup/files/sync_files.py
+++ ansible-silverblue/roles/build_cleanup/files/sync_files.py
@@ -6,6 +6,7 @@ import shutil
 os.system("rsync -avrz /etc/build_artifacts/flatpak/repo/config /var/lib/flatpak/repo/config")
 os.system("rsync -avrz /etc/build_artifacts/flatpak/.changed /var/lib/flatpak/.changed")
 os.system("rsync -avrz /etc/build_artifacts/roothome/.config /var/roothome/.config")
+os.system("rsync -avrz /etc/build_artifacts/onepassword /var/opt/1Password" )
 os.system("rsync -avrz /etc/build_artifacts/roothome/.local /var/roothome/.local")
 os.system("rsync -avrz /etc/build_artifacts/roothome/.viminfo /var/roothome/.viminfo")
 
