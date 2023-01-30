d7403e2dd9109b7aa904a2df47ddc4c46d456926
Jim Campbell <jim@ansiblefacts.com>
2023-01-29 20:34:10 -0600

    sync_files.py: Minor fixes

--- ansible-silverblue/roles/build_cleanup/files/sync_files.py
+++ ansible-silverblue/roles/build_cleanup/files/sync_files.py
@@ -3,13 +3,13 @@
 import os
 import shutil
 
-os.system("rsync -avrz /etc/build_artifacts/flatpak/repo/config /var/lib/flatpak/repo/config")
-os.system("rsync -avrz /etc/build_artifacts/flatpak/.changed /var/lib/flatpak/.changed")
-os.system("rsync -avrz /etc/build_artifacts/roothome/.config /var/roothome/.config")
-os.system("rsync -avrz /etc/build_artifacts/onepassword /var/opt/1Password" )
-os.system("rsync -avrz /etc/build_artifacts/roothome/.local /var/roothome/.local")
-os.system("rsync -avrz /etc/build_artifacts/roothome/.viminfo /var/roothome/.viminfo")
+os.system("rsync -a /etc/build_artifacts/flatpak/repo/config /var/lib/flatpak/repo/config")
+os.system("rsync -a /etc/build_artifacts/flatpak/.changed /var/lib/flatpak/.changed")
+os.system("rsync -a /etc/build_artifacts/roothome/.config /var/roothome/.config")
+os.system("rsync -a /etc/build_artifacts/roothome/.local /var/roothome/.local")
+os.system("rsync -a /etc/build_artifacts/roothome/.viminfo /var/roothome/.viminfo")
 
 shutil.rmtree('/etc/build_artifacts')
 
 os.system("echo 'The post rebase sync is complete.'")
+
